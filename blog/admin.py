437f62de6fc6e4d92fb69b08a91d4914e4ad84f1
Oskari Haikka <oskari.haikka@metropolia.fi>
2023-04-18 14:43:12 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -7,5 +7,7 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ("slug", "published_at")
 
-admin.site.register(Post, PostAdmin)
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
